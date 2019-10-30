cfd1ccb571c624d78729f9c35c79cc5fa691d0b8
Sulemana Awal <awalsulemana3@gmail.com>
2019-10-30 07:39:49 +0000

    Update secretes.js

--- src/utils/secretes.js
+++ src/utils/secretes.js
@@ -1,13 +1,13 @@
 
 
-export const FIREBASE_API_KEY="AIzaSyDqAbfwY9BWdnqE8IU5yWC9R0a2e4oe2lQ";
-export const FIREBASE_AUTHDOMAIN = "bigshot-40993.firebaseapp.com";
-export const FIREBASE_DATABASE = "https://bigshot-40993.firebaseio.com";
-export const FIREBASE_PROJECTID = "bigshot-40993";
-export const FIREBASE_STORAGE = "bigshot-40993.appspot.com";
-export const FIREBASE_MESSAGING_ID = "915182630557";
-export const FIREBASE_APP_ID = "1:915182630557:web:67572c6200831107d90674";
+export const FIREBASE_API_KEY="xxx";
+export const FIREBASE_AUTHDOMAIN = "xxx";
+export const FIREBASE_DATABASE = "xxx";
+export const FIREBASE_PROJECTID = "xxx";
+export const FIREBASE_STORAGE = "xxx";
+export const FIREBASE_MESSAGING_ID = "xxx";
+export const FIREBASE_APP_ID = "xxx";
 
-export const FACEBOOK_APP_ID= "2347564788702295";
+export const FACEBOOK_APP_ID= "xxx";
 
 export default {};
